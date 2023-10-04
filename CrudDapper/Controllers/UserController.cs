48e9735e0a8a431a08d12f6e9e36604c54721f6b
Bharath2K19 <bharathavr2k1@gmail.com>
2023-10-04 18:04:45 +0530

    First Commit

---
+++ CrudDapper/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CrudDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> GetAllUsers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            using var command = connection.QueryAsync<Users>("select * from users");
+            return Ok(command.Result);
+        }
+        [HttpGet("{UserId}")]
+
+        public async Task<ActionResult<List<Users>>> GetById(int UserId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            using var command = connection.QueryAsync<Users>($"select * from users where UserId={UserId}");
+            return Ok(command.Result);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Users>>> CreateUsers(Users user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync($"INSERT INTO Users (UserId, UserName, Email) VALUES ('{user.UserId}','{user.UserName}','{user.Email}')");
+            return Ok("Done");
+        }
+
+        
+    }
+}
---
+++ CrudDapper/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudDapper
+{
+    public class Users
+    {
+       public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
